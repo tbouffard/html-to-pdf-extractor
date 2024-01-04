ae3027bd389e41cc62cf60006337440c50476d76
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2024-01-04 17:21:55 +0100

    wip: imple

--- python/index.py
+++ python/index.py
@@ -1,8 +1,35 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, Playwright
 
-with sync_playwright() as p:
-    browser = p.chromium.launch()
+
+# with sync_playwright() as p:
+#     browser = p.chromium.launch(headless=False)
+#     page = browser.new_page()
+#     page.goto("https://github.com/tbouffard/html-to-pdf-extractor")
+#     print(page.title())
+#     print('Closing browser')
+#     browser.close()
+#     print('Browser closed')
+
+
+# Get all new pages (including popups) in the context
+def handle_page(page):
+    page.wait_for_load_state()
+    print(page.title())
+
+
+# https://playwright.dev/docs/api/class-page#page-pdf
+# https://github.com/tbouffard/html-to-pdf-extractor
+
+
+def run(p: Playwright):
+    browser = p.chromium.launch(headless=False)
     page = browser.new_page()
     page.goto("https://github.com/tbouffard/html-to-pdf-extractor")
     print(page.title())
+    print('Closing browser')
     browser.close()
+    print('Browser closed')
+
+
+with sync_playwright() as playwright:
+    run(playwright)
